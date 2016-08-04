3e8c2540a695b73820f35ed791a20348eba4b325
Manak <manak.kapoor@holmusk.com>
2016-08-04 15:01:38 +0800

    Moved from Javascript based styles to css based styles by using webpack to convert the scss styles to css instead

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
 	module: {
 		loaders: [
 		{
 			test: /\.scss$/,
-			loaders: ["style", "css", "sass"]
+			loader:ExtractTextPlugin.extract("style","css!sass")
 		}
 		]
 	},
+	plugins: [
+		new ExtractTextPlugin("./dist/[name].css")
+	],
 	entry: {
 		bundle: "./background.js",
 		tickbar: ["./tickbar/tickbar.js"]
