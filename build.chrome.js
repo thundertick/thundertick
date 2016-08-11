c50624b65ff28cacd8f6c191c4faae8325fcf74f
Manak <manak.kapoor@holmusk.com>
2016-08-11 14:30:52 +0800

    Added new scripts to switch between manifests, and a new build files to generate a zips for firefox and chrome

---
+++ build.chrome.js
@@ -0,0 +1,114 @@
+/* Gulpfile for chrome */
+var gulp = require('gulp');
+var gulpCopy = require('gulp-copy');
+var jsmin = require('gulp-jsmin');
+var cleanCSS = require('gulp-clean-css');
+var imagemin = require('gulp-imagemin');
+var clean = require('gulp-dest-clean');
+var replace = require('gulp-replace-task');
+var bump = require('gulp-bump');
+var zip = require('gulp-zip');
+var exec = require('child_process').exec;
+var fs = require('fs');
+var rename = require("gulp-rename");
+
+function existsSync(filename) {
+  try {
+    fs.accessSync(filename);
+    return true;
+  } catch(ex) {
+    return false;
+  }
+}
+gulp.task('bump:major', function(){
+  return gulp.src(['./manifest.chrome.json','./manifest.firefox.json','./package.json'])
+  .pipe(bump({type:'major'}))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump:minor', function(){
+  return gulp.src(['./manifest.chrome.json','./manifest.firefox.json','./package.json'])
+  .pipe(bump({type:'minor'}))
+  .pipe(gulp.dest('./'));
+});
+gulp.task('bump:patch', function(){
+  return gulp.src(['./manifest.chrome.json','./manifest.firefox.json','./package.json'])
+  .pipe(bump({type:'patch'}))
+  .pipe(gulp.dest('./'));
+});
+gulp.task('manifest:chrome', function(){
+	return gulp.src(['manifest.chrome.json'])
+	.pipe(rename('manifest.json'))
+	.pipe(gulp.dest(''));
+});
+
+var version = require("./manifest.chrome.json");
+version = version.version;
+if(existsSync('builds/'+version+'.chrome.zip')){
+	console.error("\033[1;31mIt seems this version ("+version+") already has a build.\n Please Bump the version, or remove the build zip file from the builds folder.\033[0m");
+	gulp.task('default', [], function(cb) {cb();});
+	return;
+}
+
+gulp.task('clean-scripts', function () {
+	return gulp.src("./", {read: false})
+	.pipe(clean("builds/chrome"));
+});
+
+gulp.task('webpack', function (cb) {
+	exec('webpack', function (err, stdout, stderr) {
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+gulp.task('movefiles', ['clean-scripts', 'webpack'], function(cb){
+	return gulp.src(['background.html','pages/help/index.html', 'pages/install/index.html', 'tickbar/font/OpenSans-Light_gdi.*'])
+	.pipe(gulpCopy('builds/chrome', {}));
+});
+
+gulp.task('removeManifestKey', ['clean-scripts', 'webpack'], function(cb){
+	return gulp.src(['manifest.chrome.json'], {base:'./'})
+	.pipe(replace({
+		patterns:[
+			{
+				match: /\"key\":.+\n/g,
+				replacement:''
+			}
+		]
+	}))
+	.pipe(rename('manifest.json'))
+	.pipe(gulp.dest('builds/chrome'));
+});
+
+gulp.task('minifyImages', ['clean-scripts', 'webpack'], function(cb){
+	return 	gulp.src(['images/*'], {base:'./'})
+	.pipe(imagemin())
+	.pipe(gulp.dest('builds/chrome'));
+});
+
+gulp.task('minifyCss', ['clean-scripts', 'webpack'], function(cb){
+	return gulp.src([ 'tickbar/font/*.css','dist/*.css'],{base: './'})
+	.pipe(cleanCSS())
+	.pipe(gulp.dest('builds/chrome'));
+});
+
+gulp.task('minifyJs', ['clean-scripts', 'webpack'], function(cb){
+	return gulp.src(['background.js','dist/*.js', 'libs/*.js'], {base: './'})
+	.pipe(jsmin())
+	.pipe(gulp.dest('builds/chrome'));
+});
+
+gulp.task('build',['movefiles','removeManifestKey','minifyImages','minifyCss','minifyJs'], function(cb){
+	console.log("Build Completed!");
+	cb();
+});
+
+gulp.task('compress',['build'], function(cb){
+	return gulp.src('builds/chrome/**/*')
+        .pipe(zip(version+'.chrome.zip'))
+        .pipe(gulp.dest('builds/'));
+});
+
+gulp.task('default', ['build', 'compress'], function(cb) {cb();});
+
--- build.firefox.js
+++ build.firefox.js
@@ -10,6 +10,8 @@ var bump = require('gulp-bump');
 var zip = require('gulp-zip');
 var exec = require('child_process').exec;
 var fs = require('fs');
+var rename = require("gulp-rename");
+
 function existsSync(filename) {
   try {
     fs.accessSync(filename);
@@ -18,26 +20,15 @@ function existsSync(filename) {
     return false;
   }
 }
-gulp.task('bump:major', function(){
-  return gulp.src(['./manifest.json','./package.json'])
-  .pipe(bump({type:'major'}))
-  .pipe(gulp.dest('./'));
+gulp.task('manifest:firefox', function(){
+	return gulp.src(['manifest.firefox.json'])
+	.pipe(rename('manifest.json'))
+	.pipe(gulp.dest(''));
 });
 
-gulp.task('bump:minor', function(){
-  return gulp.src(['./manifest.json','./package.json'])
-  .pipe(bump({type:'minor'}))
-  .pipe(gulp.dest('./'));
-});
-gulp.task('bump:patch', function(){
-  return gulp.src(['./manifest.json','./package.json'])
-  .pipe(bump({type:'patch'}))
-  .pipe(gulp.dest('./'));
-});
-
-var version = require("./manifest.json");
+var version = require("./manifest.firefox.json");
 version = version.version;
-if(existsSync('builds/'+version+'.zip')){
+if(existsSync('builds/'+version+'.firefox.zip')){
 	console.error("\033[1;31mIt seems this version ("+version+") already has a build.\n Please Bump the version, or remove the build zip file from the builds folder.\033[0m");
 	gulp.task('default', [], function(cb) {cb();});
 	return;
@@ -45,7 +36,7 @@ if(existsSync('builds/'+version+'.zip')){
 
 gulp.task('clean-scripts', function () {
 	return gulp.src("./", {read: false})
-	.pipe(clean("builds/chrome"));
+	.pipe(clean("builds/firefox"));
 });
 
 gulp.task('webpack', function (cb) {
@@ -57,38 +48,31 @@ gulp.task('webpack', function (cb) {
 
 gulp.task('movefiles', ['clean-scripts', 'webpack'], function(cb){
 	return gulp.src(['background.html','pages/help/index.html', 'pages/install/index.html', 'tickbar/font/OpenSans-Light_gdi.*'])
-	.pipe(gulpCopy('builds/chrome', {}));
+	.pipe(gulpCopy('builds/firefox', {}));
 });
 
 gulp.task('removeManifestKey', ['clean-scripts', 'webpack'], function(cb){
-	return gulp.src(['manifest.json'], {base:'./'})
-	.pipe(replace({
-		patterns:[
-			{
-				match: /\"key\":.+\n/g,
-				replacement:''
-			}
-		]
-	}))
-	.pipe(gulp.dest('builds/chrome'));
+	return gulp.src(['manifest.firefox.json'], {base:'./'})
+	.pipe(rename('manifest.json'))
+	.pipe(gulp.dest('builds/firefox'));
 });
 
 gulp.task('minifyImages', ['clean-scripts', 'webpack'], function(cb){
 	return 	gulp.src(['images/*'], {base:'./'})
 	.pipe(imagemin())
-	.pipe(gulp.dest('builds/chrome'));
+	.pipe(gulp.dest('builds/firefox'));
 });
 
 gulp.task('minifyCss', ['clean-scripts', 'webpack'], function(cb){
 	return gulp.src([ 'tickbar/font/*.css','dist/*.css'],{base: './'})
 	.pipe(cleanCSS())
-	.pipe(gulp.dest('builds/chrome'));
+	.pipe(gulp.dest('builds/firefox'));
 });
 
 gulp.task('minifyJs', ['clean-scripts', 'webpack'], function(cb){
 	return gulp.src(['background.js','dist/*.js', 'libs/*.js'], {base: './'})
 	.pipe(jsmin())
-	.pipe(gulp.dest('builds/chrome'));
+	.pipe(gulp.dest('builds/firefox'));
 });
 
 gulp.task('build',['movefiles','removeManifestKey','minifyImages','minifyCss','minifyJs'], function(cb){
@@ -97,8 +81,8 @@ gulp.task('build',['movefiles','removeManifestKey','minifyImages','minifyCss','m
 });
 
 gulp.task('compress',['build'], function(cb){
-	return gulp.src('builds/chrome/**/*')
-        .pipe(zip(version+'.zip'))
+	return gulp.src('builds/firefox/**/*')
+        .pipe(zip(version+'.firefox.zip'))
         .pipe(gulp.dest('builds/'));
 });
 
